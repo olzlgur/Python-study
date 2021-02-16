f94f89877cb2f029df2536f572f88490fee9e458
Lee Ji Heok <olzlgur@naver.com>
2021-02-16 22:50:53 +0900

    2021-02-16

---
+++ 2.Operator/1.py
@@ -0,0 +1,13 @@
+print(1+2) #3
+print(9-4) #5
+print(6/3) #2
+print(3*5) #15
+
+print(2**5) #2^5
+print(5%2) #나머지 구하기 1
+print(7/2) # 3.5
+print(7//2)  #3
+
+print(0==0) #True
+print(4==2) #False
+print(5+3==8) #True
