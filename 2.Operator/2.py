24291d4fbb787a706ea04fabe37e23bf5751c629
Lee Ji Heok <olzlgur@naver.com>
2021-02-16 22:55:38 +0900

    2021-02-16

---
+++ 2.Operator/2.py
@@ -0,0 +1,11 @@
+print(1!=3) #True
+print(not(1!=3)) #False
+
+print((3>0)and(3<5)) #True
+print((3>0)or(3<5)) #True
+
+print((3>0)&(3<5)) #True
+print((3>0)|(3>5)) #True
+
+print(10>5>0) #True
+print(10>0>5) #False
