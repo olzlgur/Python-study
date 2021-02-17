46765b9ac345dc3e554763dd93f78e93adcb013c
Lee Ji Heok <olzlgur@naver.com>
2021-02-17 13:14:57 +0900

    2021-02-17

---
+++ 2.Operator/3.py
@@ -0,0 +1,17 @@
+print(2+3*4) #14
+print((2+3)*4) #20
+
+number=2+3*4 #14
+print(number) 
+number+=2 #16
+print(number) 
+number*=2 #32
+print(number)
+number/=2 #16
+print(number)
+number-=2 #14
+print(number)
+number%=5 #4
+print(number)
+
+
