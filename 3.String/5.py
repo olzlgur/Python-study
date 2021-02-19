4af812de391f33a703dda4d381717e796a70298e
Lee Ji Heok <olzlgur@naver.com>
2021-02-19 23:06:53 +0900

    2021-02-19

---
+++ 3.String/5.py
@@ -0,0 +1,19 @@
+#\n : 줄바꿈
+print("안녕하세요.\n반갑습니다.")
+
+#저는 "이지혁"입니다. 
+# \" \' : 문장 내에서 따옴표 사용
+print('저는 "이지혁" 입니다.')
+print("저는 \"이지혁\" 입니다.")
+
+# \\ : 문장 내에서 \
+print("C:\\코드\\Python-study\3.String")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")   #PineApple 출력
+
+# \b : 백스페이스 (한 글자 삭제)
+print("Pinen\bApple")
+
+# \t : 탭
+print("Pine\tApple")
