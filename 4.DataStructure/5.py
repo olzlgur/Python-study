24334f41733f2d075b3d67ffbc4e6709253bcbe1
Lee Ji Heok <olzlgur@naver.com>
2021-02-20 18:51:26 +0900

    2021-02-20

---
+++ 4.DataStructure/5.py
@@ -0,0 +1,13 @@
+#자료구조의 변경
+
+number={1,2,3,4}
+print(number, type(number))
+
+number=list(number)
+print(number, type(number))
+
+number=tuple(number)
+print(number, type(number))
+
+number=set(number)
+print(number, type(number))
