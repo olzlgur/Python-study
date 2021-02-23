889a9bd4c240b2eff6f867533761b3c5ac0feb62
Lee Ji Heok <olzlgur@naver.com>
2021-02-23 23:11:27 +0900

    2021-02-23

---
+++ 5.Controlstatement/5.py
@@ -0,0 +1,14 @@
+student=[1,2,3,4,5]
+student_number=[i+2000 for i in student]
+
+print(student_number)
+
+study=["Java","Python","C++","C#"]
+
+study_number=[len(i) for i in study]
+
+print(study_number)
+
+Study=[i.upper() for i in study]
+
+print(Study)
