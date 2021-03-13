0097e983e45a88125427d44fe606955f8970cc35
Lee Ji Heok <olzlgur@naver.com>
2021-03-13 22:43:57 +0900

    2021-03-13

---
+++ Module/9.py
@@ -0,0 +1,18 @@
+# input : 사용자 입력을 받는 함수
+language = input("무슨 언어를 좋아하세요?")
+print("{0}은 아주 좋은 언어입니다!" .format(language))
+
+# dir : 어떤 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+print(dir())
+import random  # 외장함수
+print(dir())
+import pickle
+print (dir())
+
+print(dir(random))
+
+lis = [1,2,3]
+print(dir(list))
+
+name = "Ji"
+print(dir(name))
